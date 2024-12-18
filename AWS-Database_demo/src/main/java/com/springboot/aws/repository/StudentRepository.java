087c84b543483d8da807d75f642f06fa6302b466
Md Farhan Siddiqui <88548605+farhan1599@users.noreply.github.com>
2024-12-18 16:32:14 +0530

    Delete AWS-Database_demo directory

--- AWS-Database_demo/src/main/java/com/springboot/aws/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.aws.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.aws.entity.StudentEntity;
-
-public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
-
-}
